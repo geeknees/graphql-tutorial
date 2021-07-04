ec890b62247732c1ad8fecde1d6e4b6f2c28014a
Masumi Kawasaki <geeknees@gmail.com>
2021-07-04 13:54:01 +0900

    fix: add db constrains

--- db/migrate/20210703064428_create_links.rb
+++ db/migrate/20210703064428_create_links.rb
@@ -1,8 +1,8 @@
 class CreateLinks < ActiveRecord::Migration[6.1]
   def change
     create_table :links do |t|
-      t.string :url
-      t.text :description
+      t.string :url, null: false
+      t.text :description, null: false
 
       t.timestamps
     end
