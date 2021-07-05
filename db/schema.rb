5d4b5970ae57395f256cd0ef71fb573e6ba78ca5
Masumi Kawasaki <geeknees@gmail.com>
2021-07-05 22:28:43 +0900

    fix: add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_041902) do
+ActiveRecord::Schema.define(version: 2021_07_05_131958) do
 
   create_table "links", force: :cascade do |t|
     t.string "url", null: false
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_07_04_041902) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "links", "users"
 end
